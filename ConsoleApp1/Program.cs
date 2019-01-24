2fbe86b2dd9a76b8205f119df4bb9356b5d6c0a5
visualstudio <visualstudio@230-PC17>
2019-01-24 16:29:50 -0500

    another commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,12 +4,66 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace C0711988
 {
-    class Program
+    public class Customer
     {
-        static void Main(string[] args)
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        public void Promote()
         {
+
+            var rating = CalculateRating();
+            if (rating == 0)
+            {
+                Console.WriteLine("Promoted to Level 1");
+            }
+            else { Console.WriteLine("Promoted to Level 2"); }
+        }
+        public int CalculateRating()
+        {
+            return 0;
+        }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                //var dog_and_cat = new Car();
+                // var bowling = new Car();
+                //Console.WriteLine("how many cars : {0}", Vehicle.howManyCars);
+
+                Book myBook = new Book();
+                myBook.SetTitle(" A Separate Peace");
+                Console.WriteLine(myBook.GetTitle());
+                Console.ReadLine();
+
+            }
+
+            class Vehicle
+            {
+                public static int howManyCars = 0;
+            }
+            class Car : Vehicle
+            {
+                public String Color = "Red";
+                public Car()
+                {
+                    Console.WriteLine("my color is " + Color);
+                    Console.ReadLine();
+                }
+            }
+
+            class BookCollection
+            {
+
+            }
+            class Book : BookCollection
+            {
+                private string Title;
+                private void SetTitle(String aTitle) { this.Title = aTitle; }
+                public String GetTitle() { return this.Title; }
+            }
         }
     }
 }
